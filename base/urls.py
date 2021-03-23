62c5af3a8fc195c78faa98df92fada69d47a665a
ankrudov <lpakaco@gmail.com>
2021-03-23 13:55:08 -0500

    paths and basic html template

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 #urls cant use classes so we must use the function as_view 
 urlpatterns = [
-    path('',TaskList.as_view(),name='tasks'),
+    path('',TaskList.as_view(),name ='tasks'),
+    #view looks for the primary key in paths 'pk'
+    path('task/<int:pk>/',TaskDetail.as_view(),name ='task'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 #importing list view to return query views
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView 
 from .models import task
 
-
-#inheriting the listview functionality
+#inheriting the listview functionality, by default it looks for a task_list.html file
 class TaskList(ListView):
     model =  task
+    context_object_name = 'tasks'
+
+#by default detail view looks for task_detail.html file in the 'base' folder
+class TaskDetail(DetailView):
+    model = task
+    context_object_name = 'task' #renaming object to more easily use django logic with html 
+
+#by default this class looks for a file named task_form.html
+class TaskCreate(CreateView):
